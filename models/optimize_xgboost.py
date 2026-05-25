c7e71b2bdc2f959ca9f7b33b614d84cf38409ace
yahyalababenah <yahyalababenah@gmail.com>
2026-05-25 02:52:29 +0000

    feat: update XGBoost optimization to evaluate new engineered features using both data_clinical and data_heuristic

--- models/optimize_xgboost.py
+++ models/optimize_xgboost.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 
 def objective(trial):
     # تحميل البيانات
-    df = pd.read_csv("data/processed/final_ready_data.csv")
+    df = pd.read_csv("data/processed/data_clinical.csv")
     X = df.drop(columns=['HeartDisease']).values
     y = df['HeartDisease'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
